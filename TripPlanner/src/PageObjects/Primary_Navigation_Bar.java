2e23b7b1f555b057484537aee6d7b4e002fbf944
Shireen Abdul aziz <shireenabdulaziz@Shireens-iMac.local>
2018-10-17 23:09:13 +1000

    Implement POM

--- TripPlanner/src/PageObjects/Primary_Navigation_Bar.java
+++ TripPlanner/src/PageObjects/Primary_Navigation_Bar.java
@@ -8,17 +8,16 @@ import org.openqa.selenium.WebDriver;
 public class Primary_Navigation_Bar {
 	
 	private WebDriver driver;
-	
+	public HomeMenu homeMenu = PageFactory.initElements(driver, HomeMenu.class);;
+	public PlanMenu planMenu = PageFactory.initElements(driver, PlanMenu.class);;
+	public TicketAndOpalMenu ticketsAndOpalMenu = PageFactory.initElements(driver, TicketAndOpalMenu.class);;
+	public TravelInfoMenu travelInfoMenu = PageFactory.initElements(driver, TravelInfoMenu.class);;
+	public ContactUsMenu contactUsMenu = PageFactory.initElements(driver, ContactUsMenu.class);;
+
 	public Primary_Navigation_Bar(WebDriver driver) {
 		this.driver = driver;
 	}
 
-	public HomeMenu homeMenu = PageFactory.initElements(driver, HomeMenu.class);
-	public PlanMenu planMenu = PageFactory.initElements(driver, PlanMenu.class);
-	public TicketAndOpalMenu ticketsAndOpalMenu = PageFactory.initElements(driver, TicketAndOpalMenu.class);
-	public TravelInfoMenu travelInfoMenu = PageFactory.initElements(driver, TravelInfoMenu.class);
-	public ContactUsMenu contactUsMenu = PageFactory.initElements(driver, ContactUsMenu.class);
-	
 	@FindBy(id = "primary-nav")
 	public WebElement primaryNavBar;
 
