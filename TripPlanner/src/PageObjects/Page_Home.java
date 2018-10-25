e3356569c8b24561e4756e7a8c2a7935a5c982d8
Shireen Abdul aziz <shireenabdulaziz@Shireens-iMac.local>
2018-10-25 23:02:38 +1000

    Implement POM

--- TripPlanner/src/PageObjects/Page_Home.java
+++ TripPlanner/src/PageObjects/Page_Home.java
@@ -3,66 +3,62 @@ package PageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 
 import java.util.List;
 
+import static Common.Utilities.pickRandomWebElement;
+
 public class Page_Home {
 
-	public static class Block_TripPlanner {
+	private WebDriver driver;
 
-		public static WebElement tab_Plan(WebDriver driver) {
-
-			return driver.findElement(By.id("tridget-plan-tab"));
-
-		}
-
-		public static class Tab_Plan {
-
-			public static WebElement subtab_TripPlanner(WebDriver driver) {
-
-				return driver.findElement(By.id("tab-pane-tp"));
-
-			}
-
-			public static class SubTab_TripPlanner {
-
-				public static WebElement fromLocation(WebDriver driver) {
-					return driver.findElement(By.id("tniFromTripLocation"));
-
-				}
-
-				public static WebElement toLocation(WebDriver driver) {
-					return driver.findElement(By.id("tniToTripLocation"));
-
-				}
-
-				public static List<WebElement> autoSuggestList(WebDriver driver) {
-
-					return driver.findElements(By.xpath(".//*[contains(@id,'autosuggest-item-')]"));
-
-				}
-
-				public static WebElement goButton(WebDriver driver) {
-
-					return driver.findElement(By.xpath("//*[@id=\"tab-pane-active-content\"]/div/tni-trip-search-form/div/form/div/button[2]"));
-
-				}
-
-				public static List<WebElement> tripSearchResultList(WebDriver driver) {
-
-					return driver.findElements(By.className("tripResults tp-result-item panel panel-default ng-scope ng-isolate-scope"));
-
-				}
-
-			}
-		}
-
-		public static class Tab_Alerts {
-
-		}
-
-		public static class Book {
-
-		}
+	public Page_Home(WebDriver driver) {
+		this.driver = driver;
 	}
+
+	@FindBy(id = "tridget-plan-tab")
+	private WebElement tab_Plan;
+
+	@FindBy(id = "tniFromTripLocation")
+	private WebElement tp_TripPlanner;
+
+	@FindBy(id = "tniFromTripLocation")
+	private WebElement tp_FromLocation;
+
+	@FindBy(id = "tniToTripLocation")
+	private WebElement tp_ToLocation;
+
+	@FindBys(
+	    @FindBy(xpath = ".//*[contains(@id,'autosuggest-item-')]"))
+    private List<WebElement> searchAutoSuggestList;
+
+	@FindBy(xpath = "//*[@id=\"tab-pane-active-content\"]/div/tni-trip-search-form/div/form/div/button[2]")
+    private WebElement tp_GoButton;
+
+	@FindBys(
+    	@FindBy(className = "tripResults tp-result-item panel panel-default ng-scope ng-isolate-scope"))
+    private List<WebElement> tp_tripSearchResultsList;
+
+    public void selectTabPlan() {
+        tab_Plan.click();
+    }
+
+    public void tp_SelectTabTripPlanner(){
+        tp_TripPlanner.click();
+    }
+
+    public void tp_EnterFromLocation(String fromLocation) {
+        tp_FromLocation.sendKeys(fromLocation);
+    }
+
+    public void tp_RandomSelectLocationFromAutoSuggestList() {
+        pickRandomWebElement(searchAutoSuggestList).click();
+    }
+
+    public void tp_ClickGoButton() {
+        tp_GoButton.click();
+    }
+
 }
